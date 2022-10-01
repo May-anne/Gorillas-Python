98a31257581158a63733008c12dfc8d0ba507249
Mayanne Gomes <mayane.viviane0511@gmail.com>
2022-10-01 15:49:13 -0300

    Add codigo

---
+++ jogo/gorillas.py
@@ -0,0 +1,48 @@
+import curses
+# import time
+
+menu = ['Play', 'Ranking', 'Exit']
+
+def print_menu(stdscr, selected_row_idx):
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
+
+    stdscr.addstr(10, w//2 - len('GORILLAS')//2, 'GORILLAS')
+    
+    for idx, row in enumerate(menu):
+        x = w//2 - len(row)//2
+        y = h//2 - len(menu)//2 + idx
+        if idx == selected_row_idx:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y, x, row)
+            stdscr.attroff(curses.color_pair(1))
+        else:
+            stdscr.addstr(y, x, row)
+
+    stdscr.refresh()
+
+def main(stdscr):
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    current_row_idx = 0
+
+    print_menu(stdscr, current_row_idx)
+
+    while True:
+        key = stdscr.getch()
+        stdscr.clear()
+
+        if key == curses.KEY_UP and current_row_idx > 0:
+            current_row_idx -= 1
+        elif key == curses.KEY_DOWN and current_row_idx < len(menu) - 1:
+            current_row_idx += 1
+        elif key == curses.KEY_ENTER or key in [10, 13]:
+            stdscr.addstr(0, 0, "You pressed {}".format(menu[current_row_idx]))
+            stdscr.refresh()
+            stdscr.getch()
+
+        print_menu(stdscr, current_row_idx)
+        stdscr.refresh()
+
+curses.wrapper(main)
