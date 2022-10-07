11b9b2a412fa5c381028fd538f02b51d6d298ce7
Mayanne Gomes <mayane.viviane0511@gmail.com>
2022-10-06 22:58:12 -0300

    Comentários + borda

--- jogo/gorillas.py
+++ jogo/gorillas.py
@@ -2,13 +2,18 @@ import curses, random
 # import time
 
 menu = ['Play', 'Ranking', 'Exit']
+h = 27
+w = 154
 
+#Menu da página inicial
 def print_menu(stdscr, selected_row_idx):
     stdscr.clear()
-    h, w = stdscr.getmaxyx()
     
+    moldura(stdscr)
+
     stdscr.addstr(10, w//2 - len('GORILLAS')//2, 'GORILLAS')
     
+    #Muda a cor do texto ao descer com setas up e down
     for idx, row in enumerate(menu):
         x = w//2 - len(row)//2
         y = h//2 - len(menu)//2 + idx
@@ -22,6 +27,17 @@ def print_menu(stdscr, selected_row_idx):
     stdscr.refresh()
 
 def Playing(stdscr):
+    stdscr.clear()
+
+    '''stdscr.addstr('Informe seu nome: ')
+    curses.curs_set(1)
+    nome = stdscr.getstr(0,19, 30)
+    
+    curses.curs_set(0)
+    stdscr.clear()
+    stdscr.refresh()'''
+
+    moldura(stdscr)
 
     pad = curses.newpad(160, 200)
     stdscr.refresh()
@@ -33,34 +49,41 @@ def Playing(stdscr):
     for k in range(1):
         h = random.randint(10, 26)
         c = 0
-        pad.refresh(0, 0, h, c, 26, c+17)
+        pad.refresh(0, 0, h, c+1, 26, c+17)
         if(k==0):
             hmacaco1=h-1
             cmacaco1=int((c+17)/2)
         for m in range(8):
             h = random.randint(10, 26)
             c = c + 17
-            pad.refresh(0, 0, h, c, 26, c+17)
+            pad.refresh(0, 0, h, c+1, 26, c+17)
             if(m==7): 
                 hmacaco2=h-1
                 cmacaco2=int(c+17/2)
 
-
-        #mamaco 1
+        #Primeiro Jogador:
         stdscr.addstr(hmacaco1-2,cmacaco1+1,'o')
         stdscr.addstr(hmacaco1-1,cmacaco1,'/|\\')
         stdscr.addstr(hmacaco1,cmacaco1,'/ \\')
         
         stdscr.refresh()
         
-        #mamaco 2
+        #Segundo Jogador:
         stdscr.addstr(hmacaco2-2,cmacaco2+1,'o')
         stdscr.addstr(hmacaco2-1,cmacaco2,'/|\\')
         stdscr.addstr(hmacaco2,cmacaco2,'/ \\')
 
-        #stdscr.addstr(hmacaco2,cmacaco2,mamaco2)
         stdscr.refresh()
     stdscr.getch()
+
+#Borda do jogo
+def moldura(stdscr):
+    for i in range(27):
+        stdscr.addstr(i, 0, '#')
+        stdscr.addstr(i, w, '#')
+    for i in range(155):
+        stdscr.addstr(0, i, '#')
+        stdscr.addstr(h, i, '#')
 
 def main(stdscr):
     curses.curs_set(0)
@@ -68,9 +91,9 @@ def main(stdscr):
 
     current_row_idx = 0
     
-
     print_menu(stdscr, current_row_idx)
 
+    #Loop do Menu: espera resposta do usuário pelo teclado
     while True:
         key = stdscr.getch()
         stdscr.clear()
