805504f8c45d8a1b381ef3dbcae50a8a5963e27d
Mayanne Gomes <mayane.viviane0511@gmail.com>
2022-10-13 15:27:16 -0300

    update: salvar versão

--- jogo/gorillas.py
+++ jogo/gorillas.py
@@ -91,44 +91,20 @@ def Playing(stdscr): #Função Jogandot
             curses.noecho()
             curses.curs_set(0)
 
-            #lancamento
-            # angulo = np.deg2rad(angulo)
-
-            # bananapad = curses.newpad(160, 200)
-
-            # x=int(vel0*np.cos(angulo))
-
-            # for i in range(100):
-
-            #     for j in range(50):
-            #         curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_YELLOW)
-            #         bananapad.addstr('Z', curses.color_pair(3))
-            
-            # for i in range(100):
-                
-            #     y=int(np.tan(angulo)*x**2-0.2*x+hmacaco1-1)
-
-            #     try:
-            #         bananapad.refresh(0, 0, y, x+cmacaco1+2, y, x+cmacaco1+2)
-            #         time.sleep(0.2)
-            #     except curses.error:
-            #         pass
-            #     stdscr.refresh()
-          
-            #     x+=vel
-            #dos gerais para lançamento:
+            #Dados gerais para lançamento:
             angRAD = np.deg2rad(angulo)
             g = 10
             alcanceMax = round(((vel0**2) * np.sin(2*angRAD)) / g, 1)
             alturaMax = round((vel0**2) * (np.sin(angRAD))**2 / (2*g), 1)
             tempoTotal = round((((2*vel0) * np.sin(angRAD)) / g), 1)
-            t = np.arange(0, tempoTotal, 0.1)
+
             #PAD de lançamento:
             bananapad = curses.newpad(160, 200)
             for i in range(100):
                 for j in range(50):
                     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_YELLOW)
                     bananapad.addstr('Z', curses.color_pair(3))
+                    
             for t in np.arange(0, tempoTotal+5, 0.1):
                 x = int((cmacaco1+3) + abs(vel0) * np.cos(angRAD) * t)
                 y = int((hmacaco1-1) - (abs(vel0) * np.sin(angRAD) * t) + ((g*(t**2))/2))
